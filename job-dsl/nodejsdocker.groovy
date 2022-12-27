caf38706eaea73e513e3cb02021100260cb7b217
Waleed Mehmood <waleed.mehmood@vorihealth.com>
2022-12-27 11:56:46 -0500

    updated registery credentials

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -16,7 +16,7 @@ job('NodeJS Docker example') {
         dockerBuildAndPublish {
             repositoryName('wmfarooqi05/jenkins_node_demo')
             tag('${GIT_REVISION,length=9}')
-            registryCredentials('dockerhub')
+            registryCredentials('gitlab-deploy')
             forcePull(false)
             forceTag(false)
             createFingerprints(false)
