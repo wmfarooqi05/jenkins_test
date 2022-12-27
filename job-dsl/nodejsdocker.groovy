f758051bc74836f3af68aaa1c1dd8bf4cb0dfaa8
Waleed Mehmood <waleed.mehmood@vorihealth.com>
2022-12-27 14:12:03 -0500

    updated docker paswword id

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -16,7 +16,7 @@ job('NodeJS Docker example') {
         dockerBuildAndPublish {
             repositoryName('wmfarooqi05/jenkins_node_demo')
             tag('${GIT_REVISION,length=9}')
-            registryCredentials('gitlab-deploy')
+            registryCredentials('jenkins-docker')
             forcePull(false)
             forceTag(false)
             createFingerprints(false)
