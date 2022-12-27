8bbeddbf184063633b01fab6c406b4cf18d1a7e0
Waleed Mehmood <waleed.mehmood@vorihealth.com>
2022-12-27 11:51:58 -0500

    initial groovy file

---
+++ job-dsl/nodejsdocker.groovy
@@ -0,0 +1,26 @@
+job('NodeJS Docker example') {
+    scm {
+        git('https://github.com/wmfarooqi05/jenkins_test.git') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('Waleed Farooqi')
+            node / gitConfigEmail('wmfarooqi05@gmail.com')
+        }
+    }
+    triggers {
+        scm('H/5 * * * *')
+    }
+    wrappers {
+        nodejs('nodejs') // this is the name of the NodeJS installation in 
+                         // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
+    }
+    steps {
+        dockerBuildAndPublish {
+            repositoryName('wmfarooqi05/jenkins_node_demo')
+            tag('${GIT_REVISION,length=9}')
+            registryCredentials('dockerhub')
+            forcePull(false)
+            forceTag(false)
+            createFingerprints(false)
+            skipDecorate()
+        }
+    }
+}
